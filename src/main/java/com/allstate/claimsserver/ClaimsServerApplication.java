a28d7b8f32de9b45da71b66293b3f8f3f8cf2a0b
Vinod <vinod8840@gmail.com>
2022-08-03 13:05:18 +0000

    first commit

---
+++ src/main/java/com/allstate/claimsserver/ClaimsServerApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.claimsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allstate/claimsserver/ClaimsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.claimsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
